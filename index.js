e09f05929622e5e062a8453a67361959ab72995f
Alamin <alaminhussain32@gmail.com>
2022-05-26 16:53:30 +0600

    delete product

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run() {
             res.send(result)
         })
 
+        // delete product
+        app.delete('/products/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
         // admin
         app.put('/users/admin/:email', async (req, res) => {
